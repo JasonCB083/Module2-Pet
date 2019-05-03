be8fdaf606defa76a2421d830f7e6007978ac8ea
JasonCB083 <jason.cllr@gmail.com>
2019-05-03 18:12:51 +0200

    eslint

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -23,9 +22,9 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use((req, res, next) => {
+  next(createError(404))
+})
 
 // error handler
 app.use(function(err, req, res, next) {
