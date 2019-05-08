6f67e3374f25167e334caf5ecc2ad78194b473a4
JasonCB083 <jason.cllr@gmail.com>
2019-05-08 10:27:13 +0200

    banana

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 const mongoose = require('mongoose');
-const config = require('./config/config');
-const hbs = require('hbs');
-require('dotenv').config();
-
+const config = require('./config/config')
 
 // Session and Passport modules
-const session = require('express-session');
-const flash = require('connect-flash');
-const passport = require('./config/passport-config'); // passport module setup and initial load
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("./config/passport-config");  // passport module setup and initial load
 const passportStrategySetup = require('./config/passport-local-strategy');
 
 const router = require('./routes/index');
 // const authRoute = require('./routes/auth-routes');
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => console.log('Connected to Mongo!'))
   .catch(err => console.error('Error connecting to mongo', err));
 
@@ -27,7 +24,6 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -36,13 +32,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-
-  secret: config.SESSION_KEY,
-  resave: true,
-  saveUninitialized: true,
-  cookie: {
-    maxAge: 24 * 60 * 60 * 1000
-  }
+  secret: process.env.SESSION_KEY,
+  resave: false,
+  saveUninitialized: false
 }));
 
 // PASSPORT LINES MUST BE BELOW SESSION
